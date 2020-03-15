5ff1af3a6c4c5280e2efe488d247d77f1d4b4536
soopsio <admin@soops.io>
2020-03-15 18:35:25 +0800

    add user attrs

--- model/user.go
+++ model/user.go
@@ -1,14 +1,17 @@
 package model
 
 type User struct {
-	Base      `yaml:",inline"`
-	Password  string   `toml:"password" yaml:"password" json:"password"`
-	GroupID   int      `toml:"group_id" yaml:"group_id" json:"group_id"`
-	Directory string   `toml:"directory" yaml:"directory" json:"directory"`
-	Shell     string   `toml:"shell" yaml:"shell" json:"shell"`
-	Gecos     string   `toml:"gecos" yaml:"gecos" json:"gecos"`
-	Keys      []string `toml:"keys" yaml:"keys" json:"keys"`
-	LinkUsers []string `toml:"link_users" yaml:"link_users" json:"-"`
+	Base          `yaml:",inline"`
+	Password      string   `toml:"password" yaml:"password" json:"password"`
+	GroupID       int      `toml:"group_id" yaml:"group_id" json:"group_id"`
+	Directory     string   `toml:"directory" yaml:"directory" json:"directory"`
+	Shell         string   `toml:"shell" yaml:"shell" json:"shell"`
+	Gecos         string   `toml:"gecos" yaml:"gecos" json:"gecos"`
+	Keys          []string `toml:"keys" yaml:"keys" json:"keys"`
+	LinkUsers     []string `toml:"link_users" yaml:"link_users" json:"-"`
+	TOTP          string   `toml:"totp" yaml:"totp" json:"totp"`
+	TOTPEnable    bool     `toml:"totp_enable" yaml:"totp_enable" json:"totp_enable"`
+	SetupCommands []string `toml:"setup_commands" yaml:"setup_commands" json:"setup_commands"`
 }
 
 type Users map[string]*User
